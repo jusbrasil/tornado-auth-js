bbd7b9c6c4b6b02fac4a4194a63f5493fe7d1566
Ernesto Rocha <ernestorrocha@gmail.com>
2016-04-19 15:39:42 -0300

    Adds the encoding function (#1)
    * Added signing function from tornado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto');
+var utf8 = require('utf8');
 
 let _MAX_AGE_DAYS = 31;
 let _USER_COOKIE_NAME = 'user';
@@ -64,4 +65,17 @@ export let get_secure_cookie = (cookie_name, value, max_age_days=null) => {
 
 export let get_current_user = (value, max_age_days=null) => {
   return get_secure_cookie(_USER_COOKIE_NAME, value, max_age_days);
+}
+
+function _create_signed_value(secret, name, value) {
+  var timestamp = utf8.encode(Math.floor((new Date().getTime() / 1000)).toString());
+  var utf8_value = utf8.encode(typeof value === "string" ? value : JSON.stringify(value));
+  var value_base64 = new Buffer(utf8_value, 'utf8').toString('base64');
+  var signature = create_signature(secret, name, [value_base64, timestamp]);
+
+  return [value_base64, timestamp, signature].join('|');
+}
+
+export function create_signed_value(value) {
+  return _create_signed_value(_SECRET_KEY, _USER_COOKIE_NAME, value);
 }
